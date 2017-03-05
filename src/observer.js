90eb4bea68011bf51a7d55cba98aad08ab81ff9e
huihui <skyalpha@126.com>
2017-03-05 23:20:35 +0800

    es6 proxy core

--- src/observer.js
+++ src/observer.js
@@ -2,16 +2,35 @@
  * Define a reactive property on an Object
  */
 export default ({target, listener}) => {
-  let observable = new Proxy(target, {
-    get (target, name) {
-      return Object.freeze(target[name])
+  const handler = {
+    get (target, key, receiver) {
+      var value = Reflect.get(target, key, receiver)
+
+      if (//!(typeof value === 'object' && !!value && value.hasOwnProperty('__ob__')) &&
+        Array.isArray(value) ||
+        Object.prototype.toString.call(value) == '[object Object]' && Object.isExtensible(value)) {
+        return Observe(value)
+      }
+
+      return value
     },
-    set (target, name, value) {
-      target[name] = value
+    set (target, key, value, receiver) {
+      Reflect.set(target, key, value, receiver)
       listener(observable)
       return true
     }
-  })
+  }
+
+  const observable = Observe(target)
+
+  function Observe(target) {
+    // Object.defineProperty(target, '__ob__', {
+    //   enumerable: false,
+    //   writable: true,
+    //   configurable: true
+    // })
+    return new Proxy(target, handler)
+  }
 
   return observable
-};
+}
