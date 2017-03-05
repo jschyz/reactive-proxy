b8a30384e098f4d693751e8d5be2aceca91e25fb
huihui <skyalpha@126.com>
2017-03-05 23:24:17 +0800

    render the entry

--- src/render/index.js
+++ src/render/index.js
@@ -1,9 +1,10 @@
 import { h } from 'virtual-dom'
 
-// ui component
+// VNode:ui component
 import todo from './todo'
 import todoList from './todoList'
+import overview from './overview'
 
 export default (state) => {
-  return h('.todoapp', {}, [todo(state), todoList(state)])
+  return h('.todoapp', [todo(state), todoList(state), overview(state)])
 }
