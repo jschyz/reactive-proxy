319ae5650fbe91c559ee8c07a9d434bd61608ecd
huihui <skyalpha@126.com>
2017-03-05 23:24:34 +0800

    add LocalStorage

---
+++ src/storage.js
@@ -0,0 +1,11 @@
+// localStorage 存储 todos
+const STORAGE_KEY = 'es6-proxy'
+
+export default {
+  fetch: function () {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
