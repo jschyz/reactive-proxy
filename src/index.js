2e31761ac4f8dab5265cf3f594624aa35dba24ca
huihui <skyalpha@126.com>
2017-03-05 23:25:36 +0800

    webpack entry

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { create, patch, diff } from 'virtual-dom'
 import observer from './observer'
 // VNode (View)
 import render from './render'
+// localStorage
+import storage from './storage'
 
 // 数据状态 (Model)
 let data = {
   newTodo: '',
-  todos: []
-};
+  todos: storage.fetch(),
+  editedTodo: null,
+  visibility: 'all'
+}
 
 // 监听数据变动
 const state = observer({
@@ -20,10 +24,28 @@ const state = observer({
 
     tree = newTree
     rootNode = patch(rootNode, patches)
+
+    // watch todos change for localStorage persistence
+    storage.save(state.todos)
   }
 })
 
 let tree = render(state)
 let rootNode = create(tree)
-
+window.state = state
+// mount
 document.querySelector('#app').appendChild(rootNode)
+
+// hash 路由设置
+window.addEventListener('hashchange', () => {
+  let hash = window.location.hash.replace(/#\/?/, '')
+  if (['all', 'active', 'completed'].indexOf(hash) >= 0) {
+    state.visibility = hash
+  } else {
+    window.location.hash = ''
+    state.visibility = 'all'
+  }
+})
+// 自执行，加载触发事件
+var event = new Event('hashchange')
+window.dispatchEvent(event)
