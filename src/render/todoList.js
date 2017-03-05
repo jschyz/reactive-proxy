e8dfc534dc6d9d04721b92d0777be51a4189fac9
huihui <skyalpha@126.com>
2017-03-05 23:22:25 +0800

    The view layer: todoList

--- src/render/todoList.js
+++ src/render/todoList.js
@@ -1,40 +1,78 @@
 import { h } from 'virtual-dom'
+import { filterTodos, display } from './helper'
 
 export default (state) => {
   // is an array
-  let todos = state.todos
+  let todos = filterTodos(state)
 
-  let toggleAll = h('input.toggle-all', {
-    type: 'checkbox',
-    onclick (event) {
-      todos.forEach(todo => {
-        todo.completed = event.target.checked
-      })
-    }
-  })
-  
+  // VNode: todo list
   let elements = todos.map((todo, index) => {
-    return h('li.todo', {}, [
-      h('.view', {}, [
+    let beforeEditCache
+
+    return h('li.todo', { className: (todo.completed ? ' completed' :  '') + (state.editedTodo == todo ? ' editing' : '') }, [
+      // VNode: input[text]
+      h('.view', [
         h('input.toggle', {
           type: 'checkbox',
-          checked: todo.completed
+          checked: todo.completed,
+          onclick (event) {
+            todo.completed = event.target.checked
+          }
         }),
-        h('label', {}, [todo.title]),
+        h('label', {
+          // 双击编辑editTodo(todo)
+          ondblclick () {
+            beforeEditCache = todo.title
+            state.editedTodo = todo
+          }
+        }, [todo.title]),
         h('button.destroy', {
+          // 移除 todo
           onclick () {
-            todos = [...todos.slice(0, index), ...todos.slice(index + 1)]
+            state.todos.splice(index, 1)
           }
         })
       ]),
-      h('input.edit', {type: 'text'})
+      // VNode: input.done
+      h('input.edit', {
+        type: 'text',
+        value: todo.title,
+        onkeyup (event) {
+          // 完成编辑
+          if (event.keyCode == 13) {
+            if (!state.editedTodo) return
+
+            state.editedTodo = null
+            todo.title = todo.title.trim()
+            if (!todo.title) {
+              state.todos.splice(index, 1)
+            }
+          }
+          // 取消编辑
+          else if (event.keyCode == 27) {
+            state.editedTodo = null
+            todo.title = beforeEditCache
+          }
+        },
+        onblur () {
+          state.editedTodo = null
+          todo.title = beforeEditCache
+        }
+      })
     ])
   })
 
-  return h('section.main', {
-    style: 'display:' + ['none', 'block'][Number(!!todos.length)]
-  }, [
-    toggleAll,
+  return h('section.main', { style: display(todos.length) }, [
+    // VNode: toggle-all
+    h('input.toggle-all', {
+      type: 'checkbox',
+      onclick (event) {
+        todos.forEach(todo => {
+          todo.completed = event.target.checked
+        })
+      }
+    }),
+    // VNode: todo-list
     h('ul.todo-list', {}, elements)
   ])
 }
