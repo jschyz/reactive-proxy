19bc3977e327616c33ed6215a722e76f5cfecdb8
huihui <skyalpha@126.com>
2017-03-05 23:23:37 +0800

    Methods collection

---
+++ src/render/helper.js
@@ -0,0 +1,26 @@
+// todos 过滤器, 跟 visibility 相关
+export const filters = {
+  all: function (todos) {
+    return todos
+  },
+  active: function (todos) {
+    return todos.filter(function (todo) {
+      return !todo.completed
+    })
+  },
+  completed: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.completed
+    })
+  }
+}
+
+// computed
+export function filterTodos (state) {
+  return filters[state.visibility](state.todos)
+}
+
+//
+export function display(expression) {
+  return 'display:' + ['none', 'block'][Number(!!expression)]
+}
